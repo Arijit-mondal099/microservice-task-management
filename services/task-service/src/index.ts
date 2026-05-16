ac1eb5b9e8415677acb009466c8787a1d7226370
Arijit Mondal <arijitm717@gmail.com>
2026-05-16 10:05:46 +0530

    feat: integrate RabbitMQ connection in task service startup

--- services/task-service/src/index.ts
+++ services/task-service/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import http from "node:http";
 import { app } from "./app";
 import { connectToDatabase } from "./lib/dbConnection";
+import { connectRabbitmq } from "./lib/rabbitmq";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4001;
 
@@ -9,6 +10,7 @@ async function startServer() {
   const server = http.createServer(app);
   server.listen(PORT, async () => {
     await connectToDatabase();
+    await connectRabbitmq();
     console.log(`Task service running on port ${PORT}...`);
     console.log(`http://localhost:${PORT}`);
   });
