fe42191622e437435400b26628fc38d16f584d77
Arijit Mondal <arijitm717@gmail.com>
2026-05-16 10:05:27 +0530

    feat: integrate message consumption in notification service startup

--- services/notification-service/src/index.ts
+++ services/notification-service/src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { app } from "./app";
+import { consumeMessages } from "./lib/rabbitmq";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4002;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Notification service running on port ${PORT}`);
+  await consumeMessages();
   console.log(`http://localhost:${PORT}`);
 });
